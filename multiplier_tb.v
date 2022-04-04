b97751b310a594d6af8fce281fc6757fede5225e
xzy <xzyxzy0224@outlook.com>
2022-04-04 17:21:44 +0800

    test

--- multiplier_tb.v
+++ multiplier_tb.v
@@ -53,22 +53,22 @@ module tb_multi_seq();
 		$monitor("@ time=%0t,  A=%d, B=%d, M=%d",$time, A, B, M);
 	end
 
-    always @ ( posedge clk or negedge rst_n )
-	if( !rst_n )
-            begin
-				i <= 0;
-				en <= 0;
-                A <= 0;
-				B <= 0;			 
-            end				
-	else begin
-		for (i= -128; i<= 127; i=i+1) begin
-			for(j= -128; j<=127; j=j+1) begin
-				if( done ) begin en <= 0; i <= i + 1'b1; if (M != i*j) $display("Error: M=%d", M); end
-				else begin A <= i; B <= j; en <= 1; end
+    always @ ( posedge clk or negedge rst_n ) begin
+		if( !rst_n )begin
+			i <= 0;
+			en <= 0;
+			A <= 0;
+			B <= 0;			 
+		end				
+		else begin
+			for (i= -128; i<= 127; i=i+1) begin
+				for(j= -128; j<=127; j=j+1) begin
+					if( done ) begin en <= 0; i <= i + 1'b1; if (M != i*j) $display("Error: M=%d", M); end
+					else begin A <= i; B <= j; en <= 1; end
+				end
 			end
+		$finish;
 		end
-	$finish;
 	end
 
 
