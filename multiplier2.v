ee02ae8f941c481da630fb014052f0b1d722e221
xzy <xzyxzy0224@outlook.com>
2022-04-04 13:59:12 +0800

    test

--- multiplier2.v
+++ multiplier2.v
@@ -3,17 +3,17 @@ module booth_mult#(parameter width=8)(
      input clk,
 	 input rst_n,
 	 
-	 input [width-1:0]A,
-	 input [width-1:0]B,
+	 input [width-1:0]mul_A,
+	 input [width-1:0]mul_B,
 	 
 	 output reg done,
-	 output reg [2*width-1:0]M
+	 output reg [2*width-1:0]Product
 );
 	 
 	 reg [1:0]					state;
-	 reg [2*width-1:0]			mult_A;  // result of A
-	 reg [width:0]				mult_B;  // result of A
-	 reg [2*width-1:0]			inv_A;  // reverse result of A
+	 reg [2*width-1:0]			mult_A;  // result of mul_A
+	 reg [width:0]				mult_B;  // result of mul_A
+	 reg [2*width-1:0]			inv_A;  // reverse result of mul_A
 	 reg  [2*width-1:0]			result_tmp;  // operation register 
 	 wire [1:0]					booth_code;
 	 assign booth_code = mult_B[1:0];
@@ -26,15 +26,15 @@ module booth_mult#(parameter width=8)(
 			inv_A <= 0;
 			result_tmp  <= 0;
 			done <= 0;
-			M<=0;
+			Product<=0;
 		end
         else
             case( state )	
                 0: begin 
-                    mult_A <= {{width{A[width-1]}},A}; 
-                    inv_A <= ~{{width{A[width-1]}},A} + 1'b1 ; 
+                    mult_A <= {{width{mul_A[width-1]}},mul_A}; 
+                    inv_A <= ~{{width{mul_A[width-1]}},mul_A} + 1'b1 ; 
                     result_tmp <= 0; 
-                    mult_B <= {B,1'b0};
+                    mult_B <= {mul_B,1'b0};
                     state <= state + 1'b1; 
                 end
                 1: begin
@@ -53,7 +53,7 @@ module booth_mult#(parameter width=8)(
                 end
                 2:begin
                     done<=1'b1;
-                    M<= result_tmp;
+                    Product<= result_tmp;
                     state <= state+1;
                 end
                 3: begin
